f71875ebd734ddbb1bfe189697ae3c0fbc69fb58
unknown <tjtik83@gmail.com>
2021-04-12 19:47:30 +0700

    Project Setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Friend List</h1>
+    <form action="">
+      <table>
+        <tr>
+          <td>Name:</td>
+          <td><input type="text" v-model="friend.name" /></td>
+        </tr>
+        <tr>
+          <td>Phone:</td>
+          <td><input type="text" v-model="friend.phone" /></td>
+        </tr>
+        <tr>
+          <td>E-mail:</td>
+          <td><input type="text" v-model="friend.email" /></td>
+        </tr>
+        <tr>
+          <td><button type="submit" @click="sendData">Send</button></td>
+          <td><button type="reset" @click="clearData">Clear</button></td>
+        </tr>
+      </table>
+    </form>
+
+    <h3>{{ friend }}</h3>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      friend: {
+        name: "",
+        phone: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    sendData(e) {
+      e.preventDefault();
+      console.log("Active");
+      this.clearData();
+    },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    clearData() {
+      this.friend.name = "";
+      this.friend.phone = "";
+      this.friend.email = "";
+    },
+  },
+};
+</script>
